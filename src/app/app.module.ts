9097a8bb91412d7c67942cb4590502b0d502eccb
AlexAraujoDeBraga <alexbraga107@gmail.com>
2023-02-16 00:19:22 -0300

    componentes e rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SearchComponent } from './shared/components/search/search.component';
+import { CardHeroComponent } from './shared/components/card-hero/card-hero.component';
+import { HeroDetailComponent } from './pages/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SearchComponent,
+    CardHeroComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

7473cb0e76947f3fff78a6e2ac308c2c9e8a83bb
AlexAraujoDeBraga <alexbraga107@gmail.com>
2023-02-17 16:00:44 -0300

    card desktop style

--- src/app/core/services/heroAPI.service.ts
+++ src/app/core/services/heroAPI.service.ts
@@ -9,7 +9,7 @@ export class HeroAPIService {
 
   constructor(private httpClient: HttpClient) { }
 
-  url = 'https://gateway.marvel.com/v1/public/characters?limit=10&ts=1&apikey=25a6983c4d853b1668275832868e5ece&hash=f9f97706164898c7334e4cd3c6d3a3fd';
+  url = 'https://gateway.marvel.com/v1/public/characters?limit=40&ts=1&apikey=25a6983c4d853b1668275832868e5ece&hash=f9f97706164898c7334e4cd3c6d3a3fd';
 
   getAllHeroes():Observable<any>{
     return this.httpClient.get(this.url);
--- src/app/shared/components/card-hero/card-hero.component.ts
+++ src/app/shared/components/card-hero/card-hero.component.ts
@@ -21,5 +21,4 @@ export class CardHeroComponent implements OnInit {
     )
 
   }
-  // this.heroes = next.data.results
 }
